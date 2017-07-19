7e5e2a4cb90b0719a3566a705ff44aefb9ef73bb
yarellylopezpineda1 <yarellylopezpineda1@gmail.com>
2017-07-19 10:49:31 -0400

    funcion para sumar

--- ejemplo/Main.java
+++ ejemplo/Main.java
@@ -4,5 +4,12 @@
     
     public static void main(String[] args){
       System.out.println("Hola Mundo");
+      System.out.println("La suma de 20 y 30 es: ");
+      int s = suma(20, 30);
+      System.out.println(s);
+    }
+    
+    public static int suma(int nu, int nd){
+      return nu + nd;
     }
   }
