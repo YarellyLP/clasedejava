466855ccc9e529e9f5ed8e6f0783b7206df90e61
yarellylopezpineda1 <yarellylopezpineda1@gmail.com>
2017-07-19 11:00:24 -0400

    funcion para restar

--- ejemplo/Main.java
+++ ejemplo/Main.java
@@ -7,9 +7,16 @@
       System.out.println("La suma de 20 y 30 es: ");
       int s = suma(20, 30);
       System.out.println(s);
+      System.out.println("La resta de 20 y 10 es: ");
+       int r = resta(20, 10);
+      System.out.println(r);
     }
     
     public static int suma(int nu, int nd){
       return nu + nd;
+    }
+    
+    public static int resta(int n1, int n2){
+      return n1 - n2;
     }
   }
