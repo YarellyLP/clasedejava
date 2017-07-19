6a7ef8f5971f368f2dea2933f71888b3c021be38
yarellylopezpineda1 <yarellylopezpineda1@gmail.com>
2017-07-19 11:04:45 -0400

    funcion para multiplicar

--- ejemplo/Main.java
+++ ejemplo/Main.java
@@ -4,19 +4,29 @@
     
     public static void main(String[] args){
       System.out.println("Hola Mundo");
+      
       System.out.println("La suma de 20 y 30 es: ");
       int s = suma(20, 30);
       System.out.println(s);
+      
       System.out.println("La resta de 20 y 10 es: ");
        int r = resta(20, 10);
       System.out.println(r);
+      
+      System.out.println("La multiplicacion de 20 y 10 es: ");
+       int m = mult(20, 10);
+      System.out.println(m);
     }
     
     public static int suma(int nu, int nd){
       return nu + nd;
     }
     
-    public static int resta(int n1, int n2){
-      return n1 - n2;
+    public static int resta(int nu, int nd){
+      return nu - nd;
+    }
+    
+     public static int mult(int nu, int nd){
+      return nu * nd;
     }
   }
